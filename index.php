ec8b8a8bec29000ab6552d0fceb317f2d52e0a4e
Khaue Tavares <99762206+KhaueFacklam@users.noreply.github.com>
2023-12-01 14:24:59 -0300

    -

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>Benchmark Games</title>
+</head>
+
+<body>
+    <fieldset>
+        <legend> Inicio </legend>
+        <form name="entrada">
+
+            <a href="loginADM.php"><button type="button" name="LoginADM"> Login de Administrador </button> </a>
+            <a href="loginUser.php"><button type="button" name="LoginUser"> Login de Usuario</button></a>
+
+            <a href="cadastroUser.php"><button type="button" name="Cadastro"> Cadastro de Usuario</button></a>
+
+        </form>
+    </fieldset>
+</body>
+
+</html>
