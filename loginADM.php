1aa40c91d1d9399258a0657290fcbe0c092e9005
Pedro Bersch <pedroleottebersch@gmail.com>
2023-12-02 17:54:21 -0300

    Upload Back-end (p2)

--- loginADM.php
+++ loginADM.php
@@ -1,20 +1,39 @@
+<?php
+	session_start();
+	$resp = "";
+	$usuario = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
+	if($usuario){
+		if(in_array('',$usuario)){
+           $resp = "Preencha todos os campos!";
+       	} else {
+			$encontrar = false;
+            if("admin" == $usuario["nome"] && "admin" == $usuario["senha"]) {
+                $_SESSION["user"] = $usuario;
+                setcookie("logado","Logado",time()+60*60);
+                $encontrar = true;
+            }
+            if($encontrar){
+                header('Location: logadoADM.php');
+            } else {
+                $resp = "Administrador não encontrado!";
+            }
+        }
+    }
+?>
 <html>
-
-<head>
-    <title>Benchmark Games - Login</title>
-</head>
-
-<body>
-    <form name="login" method="POST">
-        <fieldset>
+    <head>
+        <title>Benchmark Games - Login</title>
+    </head>
+    <body>
+        <form name="login"  method="POST">
+            <fieldset>
             <legend> Login de Administrador</legend>
-            <label> Usuario: </label> <input type="text" name="nome" placeholder="Digite seu nome de usuario"><br>
-            <label> Senha: </label> <input type="password" name="senha" placeholder="Digite sua senha"><br>
-            <input type="submit" name="Enviar">
-            <h3><?= $resp; ?></h3>
-        </fieldset>
-    </form>
-</body>
-
+                <label> Usuario: </label> <input type="text" name="nome" placeholder="Digite seu nome de usuario"><br>
+                <label> Senha: </label> <input type="password" name="senha" placeholder="Digite sua senha"><br>
+                <input type="submit" name="Enviar">
+                <h3><?= $resp; ?></h3>
+            </fieldset>
+        </form>
+    </body>
 </html>
