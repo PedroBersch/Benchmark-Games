73f418fad66c4bef916a7fa0b57672df0c6b244c
Khaue Tavares <99762206+KhaueFacklam@users.noreply.github.com>
2023-12-04 14:50:33 -0300

    Melhorias 2

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
         <legend> INÍCIO </legend>
         <form name="entrada">
 
-            <nav>
+            <div>
                 <a href="loginADM.php"><button type="button" name="LoginADM"> Login de Administrador </button> </a>
                 <a href="loginUser.php"><button type="button" name="LoginUser"> Login de Usuario </button></a>
                 <a href="cadastroUser.php"><button type="button" name="Cadastro"> Cadastro de Usuario </button></a>
-            </nav>
+            </div>
 
         </form>
     </fieldset>
